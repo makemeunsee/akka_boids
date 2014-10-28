0d145b6e11c5109d7e41c6b5907158fdf47a1562
makemeunsee <makemeunsee@gmail.com>
2014-10-28 13:00:43 +0100

    better obstacle handling with mouse

--- src/main/scala/boid/twoD/UI2D.scala
+++ src/main/scala/boid/twoD/UI2D.scala
@@ -6,7 +6,7 @@ import akka.actor.{ActorRef, Actor}
 import boid.{World, Boid, Hunter}
 
 import scala.swing._
-import scala.swing.event.{UIElementResized, MouseClicked, MouseMoved}
+import scala.swing.event._
 
 /**
  * Created by markus on 25/10/2014.
@@ -48,6 +48,9 @@ class UI2D extends Actor { actor =>
           g.fillOval(heading.x.toInt-2, heading.y.toInt-2, 4, 4)
         }
         g.setColor(Color.ORANGE)
+        draggedHunter foreach { case h =>
+          g.fillOval(h.position.x.toInt-6, h.position.y.toInt-6, 13, 13)
+        }
         hunters foreach { case (h, p) =>
           g.fillOval(p.x.toInt-6, p.y.toInt-6, 13, 13)
         }
@@ -55,23 +58,51 @@ class UI2D extends Actor { actor =>
 
       listenTo(mouse.moves, mouse.clicks, this)
 
+      private var draggedHunter: Option[Hunter[Position2D]] = None
+
       reactions += {
         case MouseMoved(_, pt, _) =>
           actor.self ! World.AddHunter(new Hunter[Position2D](World.hunterBaseID, Direction2D.default, Position2D(pt.x, pt.y)))
 
-        case e@MouseClicked(_, pt, _, _, _) =>
+        case e@MouseClicked(_, pt, _, _, _) if e.peer.getButton == 3 =>
+          val p = Position2D(pt.x, pt.y)
+          hunters
+            .filter { case(h, pH) => pH.distanceTo(p) <= 10 }
+            .foreach { case(h, _) =>
+              actor.self ! World.RemoveHunter(h)
+              hunters = hunters - h
+            }
+
+        case e@MousePressed(_, pt, _, _, _) if e.peer.getButton == 1 =>
           val p = Position2D(pt.x, pt.y)
           val atPosition = hunters.filter { case(h, pH) => pH.distanceTo(p) <= 10 }
           if (atPosition.isEmpty) {
             val p = Position2D(pt.x, pt.y)
             val h = new Hunter[Position2D](nextHunterId(), Direction2D.default, p)
-            actor.self ! World.AddHunter(h)
-            hunters = hunters + ((h, p))
+            draggedHunter = Some(h)
           } else {
             atPosition.foreach { case(h, _) =>
               actor.self ! World.RemoveHunter(h)
               hunters = hunters - h
+              draggedHunter = Some(h)
             }
+          }
+
+        case e@MouseReleased(_, pt, _, _, _) if e.peer.getButton == 1 =>
+          draggedHunter foreach { h =>
+            val p = Position2D(pt.x, pt.y)
+            val newH = new Hunter[Position2D](h.id, Direction2D.default, p)
+            actor.self ! World.AddHunter(newH)
+            hunters = hunters + ((newH, p))
+          }
+          draggedHunter = None
+
+        case MouseDragged(_, pt, _) =>
+          draggedHunter match {
+            case Some(h) =>
+              draggedHunter = Some(new Hunter(h.id, Direction2D.default, Position2D(pt.x, pt.y)))
+            case _ =>
+              // nothing
           }
 
         case UIElementResized(src) =>
@@ -79,7 +110,7 @@ class UI2D extends Actor { actor =>
       }
     }
 
-    override def closeOperation: Unit = {
+    override def closeOperation(): Unit = {
       actor.self ! World.Stop
       System.exit(0)
     }
